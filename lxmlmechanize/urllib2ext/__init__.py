fccfc77fffa264b209bd8896e35801330fad1913
Moriyoshi Koizumi <mozo@mozo.jp>
2013-07-03 19:06:01 +0900

    Python 2.5 fix.

--- lxmlmechanize/urllib2ext/__init__.py
+++ lxmlmechanize/urllib2ext/__init__.py
@@ -1,1 +1,17 @@
-from .auth import *
+from .auth import (
+    HTTPClientWarning,
+    HTTPClientAuthWarning,
+    AuthAttributeParseError,
+    Credentials,
+    KeyChain,
+    AuthRequestResponse,
+    HasherRegistry,
+    AuthRequestResponder,
+    AuthRequestResponderRegistry,
+    BasicAuthRequestResponder,
+    SensibleRequest,
+    BasicAuthSensibleRequest,
+    DigestAuthRequestResponder,
+    KeyChainBackedAuthHandler,
+    )
+
